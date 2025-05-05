06c26cd609ae43aead775f5da2aa436ccc5df814
nkartikaxioned <kartikn@axioned.com>
2025-05-05 19:11:51 +0530

    conditional rendering

--- src/weather.jsx
+++ src/weather.jsx
@@ -3,6 +3,11 @@ import { WeatherInput } from "./weatherInput";
 import { GoSearch } from "react-icons/go";
 import { ChevronRight } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { FaTemperatureFull } from "react-icons/fa6";
+import { WiHumidity } from "react-icons/wi";
+import { FaWind } from "react-icons/fa6";
+import { AiFillDashboard } from "react-icons/ai";
+
 
 export const Weather = () => {
 
@@ -41,7 +46,9 @@ export const Weather = () => {
         </Button>
       </form>
       <div>
-        <h1 className="text-white">{weather?.name}, {weather?.sys?.country}</h1>
+        {weather?.weather && weather.sys.country.length > 0 && (
+          <h1 className="text-white">{weather?.name}, {weather?.sys?.country}</h1>
+        )}
         {weather?.weather && weather.weather.length > 0 && (
           <span className="text-white">{weather.weather[0].main}</span>
         )}
@@ -49,21 +56,27 @@ export const Weather = () => {
           <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} />
         )}
         <div>
+        {weather?.main?.temp && weather.main.temp.length > 0 && (
           <span className="text-white">{weather?.main?.temp}</span>
+        )}
+        {weather?.main && weather?.wind && (
           <ul className="bg-[#1e1e1e]">
             <li>
+              <FaTemperatureFull color="white" size={25}/>
               <div>
                 <span className="text-white">Feels Like</span>
                 <span className="text-white">{weather?.main?.feels_like}</span>
               </div>
             </li>
             <li>
+              <WiHumidity color="white" size={25}/>
               <div>
                 <span className="text-white">Humidity</span>
                 <span className="text-white">{weather?.main?.humidity}</span>
               </div>
             </li>
             <li>
+              <FaWind color="white" size={25}/>
               <div>
                 <span className="text-white">Wind</span>
                 <span className="text-white">{weather?.wind?.speed}</span>
@@ -71,11 +84,13 @@ export const Weather = () => {
             </li>
             <li>
               <div>
+                <AiFillDashboard color="white" size={25}/>
                 <span className="text-white">pressure</span>
                 <span className="text-white">{weather?.main?.pressure}</span>
               </div>
             </li>
           </ul>
+        )}
         </div>
       </div>
     </div>
