5cc068583ecffdb64686d1da1faebf2fb4510cfa
nkartikaxioned <kartikn@axioned.com>
2025-05-05 19:25:42 +0530

    added conditional rendering and formatting

--- src/weather.jsx
+++ src/weather.jsx
@@ -13,6 +13,7 @@ export const Weather = () => {
 
   const [stateValue, setStateValue] = useState("");
   const [weather, setWeather] = useState("");
+  const [response, setResponse] = useState(false)
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -24,18 +25,23 @@ export const Weather = () => {
       setStateValue("");
       try {
         const response = await fetch(api);
+        if (!response.ok) throw new Error('Failed to fetch weather');
+        console.log(response)
         const data = await response.json();
-        console.log(data);
+        setResponse(!false);
         setWeather(data);
       } catch (error) {
-        
+        console.log(error)
+        setResponse(!true);
       }
     }
   }
 
   return (
     <div className="bg-black">
-      <form className="flex flex-row bg-[#1e1e1e] items-center" onSubmit={(e) => handleSubmit(e)}>
+      <form 
+        className="flex flex-row bg-[#1e1e1e] items-center" 
+        onSubmit={(e) => handleSubmit(e)}>
         <GoSearch color="white" size={24}/>
         <WeatherInput 
           stateValue = {stateValue}
@@ -45,54 +51,56 @@ export const Weather = () => {
           <ChevronRight />
         </Button>
       </form>
-      <div>
-        {weather?.weather && weather.sys.country.length > 0 && (
-          <h1 className="text-white">{weather?.name}, {weather?.sys?.country}</h1>
-        )}
-        {weather?.weather && weather.weather.length > 0 && (
-          <span className="text-white">{weather.weather[0].main}</span>
-        )}
-        {weather?.weather && weather.weather.length > 0 && (
-          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} />
-        )}
+      {response !== false &&  
         <div>
-          {weather?.main?.temp && weather.main.temp.length > 0 && (
-            <span className="text-white">{weather?.main?.temp}</span>
+          {weather?.weather && weather.sys.country.length > 0 && (
+            <h1 className="text-white">{weather?.name}, {weather?.sys?.country}</h1>
           )}
-          {weather?.main && weather?.wind && (
-            <ul className="bg-[#1e1e1e]">
-              <li>
-                <FaTemperatureFull color="white" size={25}/>
-                <div>
-                  <span className="text-white">Feels Like</span>
-                  <span className="text-white">{weather?.main?.feels_like}</span>
-                </div>
-              </li>
-              <li>
-                <WiHumidity color="white" size={25}/>
-                <div>
-                  <span className="text-white">Humidity</span>
-                  <span className="text-white">{weather?.main?.humidity}</span>
-                </div>
-              </li>
-              <li>
-                <FaWind color="white" size={25}/>
-                <div>
-                  <span className="text-white">Wind</span>
-                  <span className="text-white">{weather?.wind?.speed}</span>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <AiFillDashboard color="white" size={25}/>
-                  <span className="text-white">pressure</span>
-                  <span className="text-white">{weather?.main?.pressure}</span>
-                </div>
-              </li>
-            </ul>
+          {weather?.weather && weather.weather.length > 0 && (
+            <span className="text-white">{weather.weather[0].main}</span>
           )}
+          {weather?.weather && weather.weather.length > 0 && (
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} />
+          )}
+          <div>
+            {weather?.main?.temp && weather.main.temp.length > 0 && (
+              <span className="text-white">{weather?.main?.temp}</span>
+            )}
+            {weather?.main && weather?.wind && (
+              <ul className="bg-[#1e1e1e]">
+                <li>
+                  <FaTemperatureFull color="white" size={25}/>
+                  <div>
+                    <span className="text-white">Feels Like</span>
+                    <span className="text-white">{weather?.main?.feels_like}</span>
+                  </div>
+                </li>
+                <li>
+                  <WiHumidity color="white" size={25}/>
+                  <div>
+                    <span className="text-white">Humidity</span>
+                    <span className="text-white">{weather?.main?.humidity}</span>
+                  </div>
+                </li>
+                <li>
+                  <FaWind color="white" size={25}/>
+                  <div>
+                    <span className="text-white">Wind</span>
+                    <span className="text-white">{weather?.wind?.speed}</span>
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <AiFillDashboard color="white" size={25}/>
+                    <span className="text-white">pressure</span>
+                    <span className="text-white">{weather?.main?.pressure}</span>
+                  </div>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
